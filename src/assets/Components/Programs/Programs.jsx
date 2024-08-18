96d28a5240fecb5f84934aaecfe76db7cf6852eb
Dev. Gautam Kumar <111997815+devsdenepal@users.noreply.github.com>
2024-08-18 16:27:41 +0000

    add programs slide

--- src/assets/Components/Programs/Programs.jsx
+++ src/assets/Components/Programs/Programs.jsx
@@ -1,6 +1,8 @@
 import React from 'react'
 import './Programs.css'
-import pr1 from '../pr-1.png'
+import pr1 from '../pr-1.jpg'
+import pr2 from '../pr-2.jpg'
+import pr3 from '../pr-3.jpg'
 import icon1 from '../icon1.png'
 const Programs = () => {
     return (
@@ -8,32 +10,30 @@ const Programs = () => {
             <div className="program">
                 <img src={pr1} alt="" />
                 <div className="caption">
-                    <img src={icon1} alt="" />
-                        <img src="" alt=""></img>
-                        <p>Post Graduation</p>
+                    {/* <img src={icon1} alt="" /> */}
+                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 640 512"><path fill="#ffffff" d="M96 128a128 128 0 1 1 256 0A128 128 0 1 1 96 128zM0 482.3C0 383.8 79.8 304 178.3 304l91.4 0C368.2 304 448 383.8 448 482.3c0 16.4-13.3 29.7-29.7 29.7L29.7 512C13.3 512 0 498.7 0 482.3zM609.3 512l-137.8 0c5.4-9.4 8.6-20.3 8.6-32l0-8c0-60.7-27.1-115.2-69.8-151.8c2.4-.1 4.7-.2 7.1-.2l61.4 0C567.8 320 640 392.2 640 481.3c0 17-13.8 30.7-30.7 30.7zM432 256c-31 0-59-12.6-79.3-32.9C372.4 196.5 384 163.6 384 128c0-26.8-6.6-52.1-18.3-74.3C384.3 40.1 407.2 32 432 32c61.9 0 112 50.1 112 112s-50.1 112-112 112z"/></svg>                
+                    <p>Social Skill Development</p>
+                    </div>
+            </div>
+            <div className="program">
+                <img src={pr2} alt="" />
+                <div className="caption">
+                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 576 512"><path fill="#ffffff" d="M160 0c-17.7 0-32 14.3-32 32s14.3 32 32 32l50.7 0L9.4 265.4c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0L256 109.3l0 50.7c0 17.7 14.3 32 32 32s32-14.3 32-32l0-128c0-17.7-14.3-32-32-32L160 0zM576 80a48 48 0 1 0 -96 0 48 48 0 1 0 96 0zM448 208a48 48 0 1 0 -96 0 48 48 0 1 0 96 0zM400 384a48 48 0 1 0 0-96 48 48 0 1 0 0 96zm48 80a48 48 0 1 0 -96 0 48 48 0 1 0 96 0zm128 0a48 48 0 1 0 -96 0 48 48 0 1 0 96 0zM272 384a48 48 0 1 0 0-96 48 48 0 1 0 0 96zm48 80a48 48 0 1 0 -96 0 48 48 0 1 0 96 0zM144 512a48 48 0 1 0 0-96 48 48 0 1 0 0 96zM576 336a48 48 0 1 0 -96 0 48 48 0 1 0 96 0zm-48-80a48 48 0 1 0 0-96 48 48 0 1 0 0 96z"/></svg>
+                    <p>Personal Growth</p>
 
                 </div>
             </div>
             <div className="program">
-                <img src={pr1} alt="" />
+                <img src={pr3} alt="" />
                 <div className="caption">
-                    <img src={icon1} alt="" />
-                        <img src="" alt=""></img>
-                        <p>Post Graduation</p>
-                    
+                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><path fill="#fafcff" d="M256 48C141.1 48 48 141.1 48 256l0 40c0 13.3-10.7 24-24 24s-24-10.7-24-24l0-40C0 114.6 114.6 0 256 0S512 114.6 512 256l0 144.1c0 48.6-39.4 88-88.1 88L313.6 488c-8.3 14.3-23.8 24-41.6 24l-32 0c-26.5 0-48-21.5-48-48s21.5-48 48-48l32 0c17.8 0 33.3 9.7 41.6 24l110.4 .1c22.1 0 40-17.9 40-40L464 256c0-114.9-93.1-208-208-208zM144 208l16 0c17.7 0 32 14.3 32 32l0 112c0 17.7-14.3 32-32 32l-16 0c-35.3 0-64-28.7-64-64l0-48c0-35.3 28.7-64 64-64zm224 0c35.3 0 64 28.7 64 64l0 48c0 35.3-28.7 64-64 64l-16 0c-17.7 0-32-14.3-32-32l0-112c0-17.7 14.3-32 32-32l16 0z"/></svg>
+
+                    <p>Public Speaking</p>
+
                 </div>
             </div>
-            <div className="program">
-                <img src={pr1} alt="" />
-                <div className="caption">
-                    <img src={icon1} alt="" />
-                    
-                        <p>Post Graduation</p>
-                    
-                </div>
-            </div>
-            </div>
-            )
+        </div>
+    )
 }
 
-            export default Programs
+export default Programs

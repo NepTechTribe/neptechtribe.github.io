2b5998f083894347c4db40e8cbd5c39c303e36de
Dev. Gautam Kumar <111997815+devsdenepal@users.noreply.github.com>
2024-08-18 15:38:44 +0000

    update about us

--- src/assets/Components/About/About.jsx
+++ src/assets/Components/About/About.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import './About.css'
-import Aboutimg from '../about.png'
+import Aboutimg from '../about.jpg'
 const About = () => {
     return (
         <div class="about">
@@ -9,9 +9,13 @@ const About = () => {
                 <img src="/assets/play-icon-DiRyRcRm.png" alt="" class="play-icon" />
             </div>
             <div class="about-right">
-                <h2>Nurturing Tomorrow's Leaders Today</h2>
+                <h2>Fostering Collaborative & Positive Environment</h2>
                 <br/>
-                <p>Embark on a transformative educational journey with our university's comprehensive education programs. Our cutting-edge curriculum is designed to empower students with the knowledge, skills, and experiences needed to excel in the dynamic field of education.</p><p>With a focus on innovation, hands-on learning, and personalized mentorship, our programs prepare aspiring educators to make a meaningful impact in classrooms, schools, and communities.</p><p>Whether you aspire to become a teacher, administrator, counselor, or educational leader, our diverse range of programs offers the perfect pathway to achieve your goals and unlock your full potential in shaping the future of education.</p>
+                <p>Neptech Tribe is a dynamic non-specialist organization established by a group of practical, knowledgeable and visionary young professionals with an interest in practice and education. The primary goal is to provide comprehensive digital electronics programs to organizations that include operators and equipment from a variety of backgrounds requiring irrigation equipment. Yaso gerer, neptech trib le neid seip bheka personalharolai drut rupa vikas va digital worlda garn r successful hun able banaunch.
+
+Neptech Tribe's philosophy is to believe in the central focus of knowledge, good resources, strong support, innovation, collaboration and collaborative environment, and fast-forward. The organization is a community where members live and participate freely in thought-provoking activities and contribute to collective development. Through this positive and inclusive environment that encourages creativity and innovation, we empower our members to drive meaningful change within and outside the community.
+
+Workshop, Traditional Training Sessions, March, Neptech Tribe Le Personalharoge Nayan Practical Training Sessions, Kritikta Soch Skill Development Group and Digital Economy Active Contributing Artist Bann Sikounch. With technology that is affordable and affordable, Neptech Tribal Digital is shaping the future by empowering digital leaders.</p>
             </div>
         </div>
     )

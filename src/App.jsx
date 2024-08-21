aafcc81494085bc092cc3c4820291742a9986da1
Dev. Gautam Kumar <111997815+devsdenepal@users.noreply.github.com>
2024-08-21 19:14:00 +0000

    add gallery

--- src/App.jsx
+++ src/App.jsx
@@ -5,19 +5,22 @@ import Programs from './assets/Components/Programs/Programs'
 import Title from './assets/Components/Title/Title'
 import About from './assets/Components/About/About'
 import Upcoming from './assets/Components/Upcoming/Upcoming'
+import Glimpses from './assets/Components/Glimpses/Glimpses'
 const App = () => {
   return (
     <div>
       <Navbar></Navbar>
       <Hero></Hero>
-     <div className='container'>
-     <Title title='ABOUT NEPTECH TRIBE' subtitle='What We Are'></Title>
-      <About></About>
-     <Title title='OUR PROGRAMS' subtitle='What We Offer'></Title>
-     <Programs></Programs>
-     <Title title='ANNOUNCEMENTS' subtitle='Our Upcoming Event'></Title>
-     <Upcoming></Upcoming>
-     </div>
+      <div className='container'>
+        <Title title='ABOUT NEPTECH TRIBE' subtitle='What We Are'></Title>
+        <About></About>
+        <Title title='OUR PROGRAMS' subtitle='What We Offer'></Title>
+        <Programs></Programs>
+        <Title title='ANNOUNCEMENTS' subtitle='Our Upcoming Event'></Title>
+        <Upcoming></Upcoming>
+        <Title title='Glimpses' subtitle='sneekpeek from past events'></Title>
+        <Glimpses>        </Glimpses>
+      </div>
     </div>
   )
 }
--- src/assets/Components/Programs/Programs.jsx
+++ src/assets/Components/Programs/Programs.jsx
@@ -3,7 +3,6 @@ import './Programs.css'
 import pr1 from '../pr-1.jpg'
 import pr2 from '../pr-2.jpg'
 import pr3 from '../pr-3.jpg'
-import icon1 from '../icon1.png'
 const Programs = () => {
     return (
         <div className="programs">

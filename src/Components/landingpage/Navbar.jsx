8b5b53f5f631461d0a7a51968ae0df9a06b3e5fb
Shreesha Aatreya <shreeshaaatreya1@gmail.com>
2024-09-30 07:26:23 +0545

    Nav bar of css added

--- src/Components/landingpage/Navbar.jsx
+++ src/Components/landingpage/Navbar.jsx
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import fullogo from '../../assets/LandingPage/logo.png';
 import { Link } from 'react-router-dom';
+import Container from 'react-bootstrap/Container';
 const Navbar = () => {
     const [sticky, setSticky] = useState(false);
 
@@ -18,16 +19,18 @@ const Navbar = () => {
     }, []);
 
     return (
+        <Container>
         <nav className={`Navbar ${sticky ? 'Navbar-dark' : ''}`}>
             <img src={fullogo} alt="NepTech Tribe Logo" className="Navbar-logo" />
             <ul className="Navbar-menu">
-                <li><Link to="/"  >Home</Link></li>
-                <li><Link to="/" >Gallery</Link></li>
-                <li><Link to="/">About Us</Link> </li>
-                <li><Link to="/">Upcoming Events</Link></li>
-                <li><Link to="/" >Contact us</Link></li>
+                <li><Link to="/" className='link-default' >Home</Link></li>
+                <li><Link to="/gallary" className='link-default' >Gallery</Link></li>
+                <li><Link to="/about" className='link-default'>About Us</Link> </li>
+                <li><Link to="/events" className='link-default'> Upcoming Events</Link></li>
+                <li><Link to="/contact" className='link-default' >Contact us</Link></li>
             </ul>
         </nav>
+        </Container>
     );
 };
 

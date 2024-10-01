9f514b7336662be8574a3358fd9d8648a15130d6
Shreesha Aatreya <shreeshaaatreya1@gmail.com>
2024-10-01 12:30:22 +0545

    About Section Added

--- src/Pages/Landingpage.jsx
+++ src/Pages/Landingpage.jsx
@@ -1,11 +1,13 @@
 import Navbar from "../Components/landingpage/Navbar";
 import Hero from "../Components/landingpage/hero";
+import AboutUs from "../Components/landingpage/about";
 
 function LandingPage(){
     return(
         <>
         <Navbar/>
         <Hero/>
+        <AboutUs/>
         </>
     )
 }

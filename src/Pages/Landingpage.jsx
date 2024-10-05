d8d2c0480cd41fe628993841627ab039e1a60a2c
Shreesha Aatreya <shreeshaaatreya1@gmail.com>
2024-10-05 23:36:35 +0545

    Past event section completed

--- src/Pages/Landingpage.jsx
+++ src/Pages/Landingpage.jsx
@@ -3,6 +3,7 @@ import Hero from "../Components/landingpage/hero";
 import AboutUs from "../Components/landingpage/about";
 import Programs from "../Components/landingpage/programs";
 import Announcement from "../Components/landingpage/announcement";
+import PastEvent from "../Components/landingpage/pastevent";
 
 function LandingPage(){
     return(
@@ -12,6 +13,7 @@ function LandingPage(){
         <AboutUs/>
         <Programs/>
         <Announcement/>
+        <PastEvent/>
         </>
     )
 }

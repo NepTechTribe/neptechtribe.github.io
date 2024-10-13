ea4170db597cb1329fb188a81a95dc28da3c5502
Shreesha Aatreya <shreeshaaatreya1@gmail.com>
2024-10-13 09:30:55 +0545

    Testimonials section added

--- src/Components/landingpage/button.jsx
+++ src/Components/landingpage/button.jsx
@@ -1,8 +1,20 @@
-function Button({buttontext,buttonclass , buttoncontainer}){
+import { useNavigate } from 'react-router-dom';
+
+
+
+function Button({buttontext,buttonclass , buttoncontainer ,location}){
+
+    const navigate = useNavigate();
+
+    const handleClick = () => {
+      navigate(location);
+    };
+
+
     return(
         <>
         <div className={buttoncontainer} >
-        <button className={buttonclass}>{buttontext}</button>
+        <button className={buttonclass}  onClick={handleClick}>{buttontext}</button>
         </div>
         </>
     )
